e68fa8f94ec1fd0f3264c6e17551dd675a49dfcb
David Hileman <dhileman823@gmail.com>
2020-03-10 22:39:47 -0400

    Added a user model with marshmallow schema

--- Server.py
+++ Server.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask import request
+from flask_marshmallow import Marshmallow
+from models.user import User
+from schemas.user_schema import UserSchema
 
 app = Flask(__name__)
-
-cache = {}
-
+ma = Marshmallow(app)
 
 @app.route("/")
 def server_info():
-    return "Webserver home"
+    return "API Home"
 
 
 @app.route("/users/<int:user_id>")
 def getUser(user_id):
     # get a user by id
-    return "{\"user_id\":1,\"name\":\"David\",\"position\":\"Developer\"}"
-
+    user = User(name="David", email="david.hileman@anoidapps.com")
+    schema = UserSchema()
+    result = schema.dump(user)
+    return result
