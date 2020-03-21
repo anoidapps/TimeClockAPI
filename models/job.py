404851eaead69abe1d667cffdd62a4a3de74bc02
David Hileman <dhileman823@gmail.com>
2020-03-21 13:12:15 -0400

    added address to constructor

--- models/job.py
+++ models/job.py
@@ -16,14 +16,14 @@ class Job (db.Model):
 	longitude = db.Column(db.Float, unique=False)
 
 	# Constructor
-	def __init__(self, name, description, org_id):
+	def __init__(self, name, description, address, org_id):
 		self.job_id = None
 		self.name = name
 		self.org_id = org_id
 		self.created_at = dt.datetime.now()
 
 		self.description = description
-		self.address = None
+		self.address = address
 		self.latitude = None
 		self.longitude = None
 
